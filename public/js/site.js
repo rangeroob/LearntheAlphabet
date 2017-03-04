1689c6e4439f3410ee9a30bff5440ef84e11f4f1
derekviera <ma.dmviera01@gmail.com>
2017-03-04 02:06:19 -0500

    update site.js
    put defintions in an Array
    
    functionalize 'document.getElementById("definition").innerHTML ='
    *under alphabetDefinitions

--- public/js/site.js
+++ public/js/site.js
@@ -8,168 +8,182 @@ var bang = {
             'z'
         ];
 
+        var definitions = ['A is for Apple', 'B is for Box', 'C is for Cat', 'D is for Dog', 'E is for Elephant',
+            'F is for Frog', 'G is for Girl', '"H is for Hay', 'I is for Igloo', 'J is for Joke',
+            'K is for King', 'L is for Lion', 'M is for Man', 'N is for Night', 'O is for Open',
+            'P is for Plant', 'Q is for Queen', 'R is for Room', 'S is for Soup', 'T is for Tree',
+            'U is for Up', 'V is for Violin', 'W is for Water', 'X is for Xylophone', 'Y is for Yogurt',
+            'Z is for Zebra'
+        ];
+
+
+        function alphabetDefinitions(number) {
+            return document.getElementById("definition").innerHTML = definitions[number];
+        };
+
         return m("bang", [
             m("h2.w3-center", "Learn the Alphabet"),
             m("h3.w3-center[id='definition']"),
             m(".w3-container.w3-center.w3-bar",
                 m("button.w3-btn.w3-blue.w3-padding-large", {
                         onclick: (function() {
-                            document.getElementById("definition").innerHTML = "A is for Apple"
+                            alphabetDefinitions(0)
                         })
+
                     },
                     alphabet[0]),
                 m("button.w3-btn.w3-padding-large", {
                         onclick: (function() {
-                            document.getElementById("definition").innerHTML = "B is for Box"
+                            alphabetDefinitions(1)
                         })
                     },
                     alphabet[1]),
                 m("button.w3-btn.w3-blue.w3-padding-large", {
                         onclick: (function() {
-                            document.getElementById("definition").innerHTML = "C is for Cat"
+                            alphabetDefinitions(2)
                         })
                     },
                     alphabet[2]),
                 m("button.w3-btn.w3-padding-large", {
                         onclick: (function() {
-                            document.getElementById("definition").innerHTML = "D is for Dog"
+                            alphabetDefinitions(3)
                         })
                     },
                     alphabet[3]),
 
                 m("button.w3-btn.w3-blue.w3-padding-large", {
                         onclick: (function() {
-                            document.getElementById("definition").innerHTML = "E is for Elephant"
+                            alphabetDefinitions(4)
                         })
                     },
                     alphabet[4]),
                 m("button.w3-btn.w3-padding-large", {
                         onclick: (function() {
-                            document.getElementById("definition").innerHTML = "F is for Frog"
+                            alphabetDefinitions(5)
                         })
                     },
                     alphabet[5]),
                 m("button.w3-btn.w3-blue.w3-padding-large", {
                         onclick: (function() {
-                            document.getElementById("definition").innerHTML = "G is for Girl"
+                            alphabetDefinitions(6)
                         })
                     },
                     alphabet[6]),
                 m("button.w3-btn.w3-padding-large", {
                         onclick: (function() {
-                            document.getElementById("definition").innerHTML = "H is for Hay"
+                            alphabetDefinitions(7)
                         })
                     },
                     alphabet[7]),
                 m("button.w3-btn.w3-blue.w3-padding-large", {
                         onclick: (function() {
-                            document.getElementById("definition").innerHTML = "I is for Igloo"
+                            alphabetDefinitions(8)
                         })
                     },
                     alphabet[8]),
                 m("button.w3-btn.w3-padding-large", {
                         onclick: (function() {
-                            document.getElementById("definition").innerHTML = "J is for Joke"
+                            alphabetDefinitions(9)
                         })
                     },
                     alphabet[9]),
 
                 m("button.w3-btn.w3-blue.w3-padding-large", {
                         onclick: (function() {
-                            document.getElementById("definition").innerHTML = "K is for King"
+                            alphabetDefinitions(10)
                         })
                     },
                     alphabet[10]),
                 m("button.w3-btn.w3-padding-large", {
                         onclick: (function() {
-                            document.getElementById("definition").innerHTML = "L is for Lion"
+                            alphabetDefinitions(11)
                         })
                     },
                     alphabet[11]),
                 m("button.w3-btn.w3-blue.w3-padding-large", {
                         onclick: (function() {
-                            document.getElementById("definition").innerHTML = "M is for Man"
+                            alphabetDefinitions(12)
                         })
                     },
                     alphabet[12]),
                 m("button.w3-btn.w3-padding-large", {
                         onclick: (function() {
-                            document.getElementById("definition").innerHTML = "N is for Night"
+                            alphabetDefinitions(13)
                         })
                     },
                     alphabet[13]),
 
                 m("button.w3-btn.w3-blue.w3-padding-large", {
                         onclick: (function() {
-                            document.getElementById("definition").innerHTML = "O is for Open"
+                            alphabetDefinitions(14)
                         })
                     },
                     alphabet[14]),
                 m("button.w3-btn.w3-padding-large", {
                         onclick: (function() {
-                            document.getElementById("definition").innerHTML = "P is for Plant"
+                            alphabetDefinitions(15)
                         })
                     },
                     alphabet[15]),
                 m("button.w3-btn.w3-blue.w3-padding-large", {
                         onclick: (function() {
-                            document.getElementById("definition").innerHTML = "Q is for Queen"
+                            alphabetDefinitions(16)
                         })
                     },
                     alphabet[16]),
                 m("button.w3-btn.w3-padding-large", {
                         onclick: (function() {
-                            document.getElementById("definition").innerHTML = "R is for Room"
+                            alphabetDefinitions(17)
                         })
                     },
                     alphabet[17]),
                 m("button.w3-btn.w3-blue.w3-padding-large", {
                         onclick: (function() {
-                            document.getElementById("definition").innerHTML = "S is for Soup"
+                            alphabetDefinitions(18)
                         })
                     },
                     alphabet[18]),
                 m("button.w3-btn.w3-padding-large", {
                         onclick: (function() {
-                            document.getElementById("definition").innerHTML = "T is for Tree"
+                            alphabetDefinitions(19)
                         })
                     },
                     alphabet[19]),
 
                 m("button.w3-btn.w3-blue.w3-padding-large", {
                         onclick: (function() {
-                            document.getElementById("definition").innerHTML = "U is for Up"
+                            alphabetDefinitions(20)
                         })
                     },
                     alphabet[20]),
                 m("button.w3-btn.w3-padding-large", {
                         onclick: (function() {
-                            document.getElementById("definition").innerHTML = "V is for Violin"
+                            alphabetDefinitions(21)
                         })
                     },
                     alphabet[21]),
                 m("button.w3-btn.w3-blue.w3-padding-large", {
                         onclick: (function() {
-                            document.getElementById("definition").innerHTML = "W is for Water"
+                            alphabetDefinitions(22)
                         })
                     },
                     alphabet[22]),
                 m("button.w3-btn.w3-padding-large", {
                         onclick: (function() {
-                            document.getElementById("definition").innerHTML = "X is for Xylophone"
+                            alphabetDefinitions(23)
                         })
                     },
                     alphabet[23]),
 
                 m("button.w3-btn.w3-blue.w3-padding-large", {
                         onclick: (function() {
-                            document.getElementById("definition").innerHTML = "Y is for Yogurt"
+                            alphabetDefinitions(24)
                         })
                     },
                     alphabet[24]),
                 m("button.w3-btn.w3-padding-large", {
                         onclick: (function() {
-                            document.getElementById("definition").innerHTML = "Z is for Zebra"
+                            alphabetDefinitions(25)
                         })
                     },
                     alphabet[25])
