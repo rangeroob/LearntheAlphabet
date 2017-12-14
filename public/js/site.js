fbee2b4a93acf1614feab499594224f99803d84f
derekviera <ma.dmviera01@gmail.com>
2017-12-14 00:15:34 -0500

    Add all letters to array and onclick function for sound

--- public/js/site.js
+++ public/js/site.js
@@ -36,10 +36,18 @@ var bang = {
     }
 
     function soundBytes (number) {
-      var sounds = ['http://www.soundjay.com/button/beep-07.wav']
+      var sounds = ['public/sound/67712__corsica-s__a.mp3', 'public/sound/67713__corsica-s__b.mp3', 'public/sound/67714__corsica-s__c.mp3',
+        'public/sound/67715__corsica-s__d.mp3', 'public/sound/67716__corsica-s__e.mp3', 'public/sound/67717__corsica-s__f.mp3',
+        'public/sound/67718__corsica-s__g.mp3', 'public/sound/67719__corsica-s__h.mp3', 'public/sound/67720__corsica-s__i.mp3',
+        'public/sound/67721__corsica-s__j.mp3', 'public/sound/67722__corsica-s__k.mp3', 'public/sound/67723__corsica-s__l.mp3',
+        'public/sound/67724__corsica-s__m.mp3', 'public/sound/67725__corsica-s__n.mp3', 'public/sound/67726__corsica-s__o.mp3',
+        'public/sound/67727__corsica-s__p.mp3', 'public/sound/67728__corsica-s__q.mp3', 'public/sound/67729__corsica-s__r.mp3',
+        'public/sound/67730__corsica-s__s.mp3', 'public/sound/67731__corsica-s__t.mp3', 'public/sound/67732__corsica-s__u.mp3',
+        'public/sound/67733__corsica-s__v.mp3', 'public/sound/67734__corsica-s__w.mp3', 'public/sound/67735__corsica-s__x.mp3',
+        'public/sound/67736__corsica-s__y.mp3', 'public/sound/67737__corsica-s__z.mp3']
       var getSound = document.getElementById('sound').src = sounds[number]
       document.getElementById('sound').play(getSound)
-    }
+    };
 
     return m('bang', [
       m('h2.w3-center', 'Learn the Alphabet'),
@@ -48,197 +56,222 @@ var bang = {
       m("audio[id='sound']"),
       m('br'),
       m('.w3-container.w3-center.w3-bar',
-                m('button.w3-btn.w3-blue.w3-padding-large', {
-                  onclick: function () {
-                    alphabetDefinitions(0)
-                    imageList(0)
-                    soundBytes(0)
-                  }
+        m('button.w3-btn.w3-blue.w3-padding-large', {
+          onclick: function () {
+            alphabetDefinitions(0)
+            imageList(0)
+            soundBytes(0)
+          }
 
-                },
-                    alphabet[0]),
-                m('button.w3-btn.w3-padding-large', {
-                  onclick: function () {
-                    alphabetDefinitions(1)
-                    imageList(1)
-                  }
-                },
-                    alphabet[1]),
-                m('button.w3-btn.w3-blue.w3-padding-large', {
-                  onclick: function () {
-                    alphabetDefinitions(2)
-                    imageList(2)
-                  }
-                },
-                    alphabet[2]),
-                m('button.w3-btn.w3-padding-large', {
-                  onclick: function () {
-                    alphabetDefinitions(3)
-                    imageList(3)
-                  }
-                },
-                    alphabet[3]),
+        },
+          alphabet[0]),
+        m('button.w3-btn.w3-padding-large', {
+          onclick: function () {
+            alphabetDefinitions(1)
+            imageList(1)
+            soundBytes(1)
+          }
+        },
+          alphabet[1]),
+        m('button.w3-btn.w3-blue.w3-padding-large', {
+          onclick: function () {
+            alphabetDefinitions(2)
+            imageList(2)
+            soundBytes(2)
+          }
+        },
+          alphabet[2]),
+        m('button.w3-btn.w3-padding-large', {
+          onclick: function () {
+            alphabetDefinitions(3)
+            imageList(3)
+            soundBytes(3)
+          }
+        },
+          alphabet[3]),
 
-                m('button.w3-btn.w3-blue.w3-padding-large', {
-                  onclick: function () {
-                    alphabetDefinitions(4)
-                    imageList(4)
-                  }
-                },
-                    alphabet[4]),
-                m('button.w3-btn.w3-padding-large', {
-                  onclick: function () {
-                    alphabetDefinitions(5)
-                    imageList(5)
-                  }
-                },
-                    alphabet[5]),
-                m('button.w3-btn.w3-blue.w3-padding-large', {
-                  onclick: function () {
-                    alphabetDefinitions(6)
-                    imageList(6)
-                  }
-                },
-                    alphabet[6]),
-                m('button.w3-btn.w3-padding-large', {
-                  onclick: function () {
-                    alphabetDefinitions(7)
-                    imageList(7)
-                  }
-                },
-                    alphabet[7]),
-                m('button.w3-btn.w3-blue.w3-padding-large', {
-                  onclick: function () {
-                    alphabetDefinitions(8)
-                    imageList(8)
-                  }
-                },
-                    alphabet[8]),
-                m('button.w3-btn.w3-padding-large', {
-                  onclick: function () {
-                    alphabetDefinitions(9)
-                    imageList(9)
-                  }
-                },
-                    alphabet[9]),
+        m('button.w3-btn.w3-blue.w3-padding-large', {
+          onclick: function () {
+            alphabetDefinitions(4)
+            imageList(4)
+            soundBytes(4)
+          }
+        },
+          alphabet[4]),
+        m('button.w3-btn.w3-padding-large', {
+          onclick: function () {
+            alphabetDefinitions(5)
+            imageList(5)
+            soundBytes(5)
+          }
+        },
+          alphabet[5]),
+        m('button.w3-btn.w3-blue.w3-padding-large', {
+          onclick: function () {
+            alphabetDefinitions(6)
+            imageList(6)
+            soundBytes(6)
+          }
+        },
+          alphabet[6]),
+        m('button.w3-btn.w3-padding-large', {
+          onclick: function () {
+            alphabetDefinitions(7)
+            imageList(7)
+            soundBytes(7)
+          }
+        },
+          alphabet[7]),
+        m('button.w3-btn.w3-blue.w3-padding-large', {
+          onclick: function () {
+            alphabetDefinitions(8)
+            imageList(8)
+            soundBytes(8)
+          }
+        },
+          alphabet[8]),
+        m('button.w3-btn.w3-padding-large', {
+          onclick: function () {
+            alphabetDefinitions(9)
+            imageList(9)
+            soundBytes(9)
+          }
+        },
+          alphabet[9]),
 
-                m('button.w3-btn.w3-blue.w3-padding-large', {
-                  onclick: function () {
-                    alphabetDefinitions(10)
-                    imageList(10)
-                  }
-                },
-                    alphabet[10]),
-                m('button.w3-btn.w3-padding-large', {
-                  onclick: function () {
-                    alphabetDefinitions(11)
-                    imageList(11)
-                  }
-                },
-                    alphabet[11]),
-                m('button.w3-btn.w3-blue.w3-padding-large', {
-                  onclick: function () {
-                    alphabetDefinitions(12)
-                    imageList(12)
-                  }
-                },
-                    alphabet[12]),
-                m('button.w3-btn.w3-padding-large', {
-                  onclick: function () {
-                    alphabetDefinitions(13)
-                    imageList(13)
-                  }
-                },
-                    alphabet[13]),
+        m('button.w3-btn.w3-blue.w3-padding-large', {
+          onclick: function () {
+            alphabetDefinitions(10)
+            imageList(10)
+            soundBytes(10)
+          }
+        },
+          alphabet[10]),
+        m('button.w3-btn.w3-padding-large', {
+          onclick: function () {
+            alphabetDefinitions(11)
+            imageList(11)
+            soundBytes(11)
+          }
+        },
+          alphabet[11]),
+        m('button.w3-btn.w3-blue.w3-padding-large', {
+          onclick: function () {
+            alphabetDefinitions(12)
+            imageList(12)
+            soundBytes(12)
+          }
+        },
+          alphabet[12]),
+        m('button.w3-btn.w3-padding-large', {
+          onclick: function () {
+            alphabetDefinitions(13)
+            imageList(13)
+            soundBytes(13)
+          }
+        },
+          alphabet[13]),
 
-                m('button.w3-btn.w3-blue.w3-padding-large', {
-                  onclick: function () {
-                    alphabetDefinitions(14)
-                    imageList(14)
-                  }
-                },
-                    alphabet[14]),
-                m('button.w3-btn.w3-padding-large', {
-                  onclick: function () {
-                    alphabetDefinitions(15)
-                    imageList(15)
-                  }
-                },
-                    alphabet[15]),
-                m('button.w3-btn.w3-blue.w3-padding-large', {
-                  onclick: function () {
-                    alphabetDefinitions(16)
-                    imageList(16)
-                  }
-                },
-                    alphabet[16]),
-                m('button.w3-btn.w3-padding-large', {
-                  onclick: function () {
-                    alphabetDefinitions(17)
-                    imageList(17)
-                  }
-                },
-                    alphabet[17]),
-                m('button.w3-btn.w3-blue.w3-padding-large', {
-                  onclick: function () {
-                    alphabetDefinitions(18)
-                    imageList(18)
-                  }
-                },
-                    alphabet[18]),
-                m('button.w3-btn.w3-padding-large', {
-                  onclick: function () {
-                    alphabetDefinitions(19)
-                    imageList(19)
-                  }
-                },
-                    alphabet[19]),
+        m('button.w3-btn.w3-blue.w3-padding-large', {
+          onclick: function () {
+            alphabetDefinitions(14)
+            imageList(14)
+            soundBytes(14)
+          }
+        },
+          alphabet[14]),
+        m('button.w3-btn.w3-padding-large', {
+          onclick: function () {
+            alphabetDefinitions(15)
+            imageList(15)
+            soundBytes(15)
+          }
+        },
+          alphabet[15]),
+        m('button.w3-btn.w3-blue.w3-padding-large', {
+          onclick: function () {
+            alphabetDefinitions(16)
+            imageList(16)
+            soundBytes(16)
+          }
+        },
+          alphabet[16]),
+        m('button.w3-btn.w3-padding-large', {
+          onclick: function () {
+            alphabetDefinitions(17)
+            imageList(17)
+            soundBytes(17)
+          }
+        },
+          alphabet[17]),
+        m('button.w3-btn.w3-blue.w3-padding-large', {
+          onclick: function () {
+            alphabetDefinitions(18)
+            imageList(18)
+            soundBytes(18)
+          }
+        },
+          alphabet[18]),
+        m('button.w3-btn.w3-padding-large', {
+          onclick: function () {
+            alphabetDefinitions(19)
+            imageList(19)
+            soundBytes(19)
+          }
+        },
+          alphabet[19]),
 
-                m('button.w3-btn.w3-blue.w3-padding-large', {
-                  onclick: function () {
-                    alphabetDefinitions(20)
-                    imageList(20)
-                  }
-                },
-                    alphabet[20]),
-                m('button.w3-btn.w3-padding-large', {
-                  onclick: function () {
-                    alphabetDefinitions(21)
-                    imageList(21)
-                  }
-                },
-                    alphabet[21]),
-                m('button.w3-btn.w3-blue.w3-padding-large', {
-                  onclick: function () {
-                    alphabetDefinitions(22)
-                    imageList(22)
-                  }
-                },
-                    alphabet[22]),
-                m('button.w3-btn.w3-padding-large', {
-                  onclick: function () {
-                    alphabetDefinitions(23)
-                    imageList(23)
-                  }
-                },
-                    alphabet[23]),
+        m('button.w3-btn.w3-blue.w3-padding-large', {
+          onclick: function () {
+            alphabetDefinitions(20)
+            imageList(20)
+            soundBytes(20)
+          }
+        },
+          alphabet[20]),
+        m('button.w3-btn.w3-padding-large', {
+          onclick: function () {
+            alphabetDefinitions(21)
+            imageList(21)
+            soundBytes(21)
+          }
+        },
+          alphabet[21]),
+        m('button.w3-btn.w3-blue.w3-padding-large', {
+          onclick: function () {
+            alphabetDefinitions(22)
+            imageList(22)
+            soundBytes(22)
+          }
+        },
+          alphabet[22]),
+        m('button.w3-btn.w3-padding-large', {
+          onclick: function () {
+            alphabetDefinitions(23)
+            imageList(23)
+            soundBytes(23)
+          }
+        },
+          alphabet[23]),
 
-                m('button.w3-btn.w3-blue.w3-padding-large', {
-                  onclick: function () {
-                    alphabetDefinitions(24)
-                    imageList(24)
-                  }
-                },
-                    alphabet[24]),
-                m('button.w3-btn.w3-padding-large', {
-                  onclick: function () {
-                    alphabetDefinitions(25)
-                    imageList(25)
-                  }
-                },
-                    alphabet[25])
+        m('button.w3-btn.w3-blue.w3-padding-large', {
+          onclick: function () {
+            alphabetDefinitions(24)
+            imageList(24)
+            soundBytes(24)
+          }
+        },
+          alphabet[24]),
+        m('button.w3-btn.w3-padding-large', {
+          onclick: function () {
+            alphabetDefinitions(25)
+            imageList(25)
+            soundBytes(25)
+          }
+        },
+          alphabet[25])
 
-            )
+      )
 
     ])
   }
